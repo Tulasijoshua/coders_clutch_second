706ea1628fc7fcf80e28bc86a63d3f8100f10078
Tulasijoshua <mbawinitulasi12@gmail.com>
2025-07-01 23:48:34 +0000

    feat: global network

--- components/about/global-networks/global-networks-main.tsx
+++ components/about/global-networks/global-networks-main.tsx
@@ -1,8 +1,82 @@
+'use client'
+import Typography from '@/components/shared/typography'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { brands } from '@/constant'
+import Image from 'next/image'
 import React from 'react'
 
 function GlobalNetworksMain() {
   return (
-    <div>GlobalNetworksMain</div>
+    <div className='w-full py-12 bg-[#F0FAFC]'>
+        <section className='max-container 2xl:w-[80%] w-[90%] mx-auto'>
+            <div className='flex flex-col justify-center items-center gap-'>
+                <Typography
+                    typo="body-medium-medium"
+                    className='uppercase text-app-secondary'
+                >
+                    Brands that Trust us
+                </Typography>
+                <Typography
+                    typo="header-2-semibold"
+                    className='pb-3 !font-bold'
+                >
+                    Our Clients
+                </Typography>
+                <Typography
+                    typo="body-large-regular"
+                    className='max-w-[450px] text-center'
+                >
+                    Over the years, Coders Clutch has had the privilege to
+                    work for both local and global businesses
+                </Typography>
+            </div>
+            <div className='py-12 flex flex-col gap-8 border-b'>
+                <div className='flex flex-col gap-2'>
+                    <Typography
+                        typo="header-4-semibold"
+                    >Local Clients</Typography>
+                    <div className='w-16 h-[2px] bg-app-secondary'></div>
+                </div>
+                <div className='w-full grid xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 xs:gap-6 gap-3'>
+                    {brands.map((logo, index) => (
+                        <Card
+                            key={`Brand logo ${index+1}`}
+                            className='w-full lg:h-[150px] h-[100px] flex flex-col justify-center items-center border-none bg-white shadow-sm rounded-md hover:shadow-lg transition-all duration-300'
+                        >
+                            <Image
+                                src={logo}
+                                alt={`Brand ${index+1}`}
+                                className='w-[100px] h-fit'
+                            />
+                        </Card>
+                    ))}
+                </div>
+            </div>
+            <div className='py-12 flex flex-col gap-8'>
+                <div className='flex flex-col gap-2'>
+                    <Typography
+                        typo="header-4-semibold"
+                    >Global Clients</Typography>
+                    <div className='w-16 h-[2px] bg-app-secondary'></div>
+                </div>
+                <div className='w-full grid xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 xs:gap-6 gap-3'>
+                    {brands.map((logo, index) => (
+                        <Card
+                            key={`Brand logo ${index+1}`}
+                            className='w-full lg:h-[150px] h-[100px] flex flex-col justify-center items-center border-none bg-white shadow-sm rounded-md hover:shadow-lg transition-all duration-300'
+                        >
+                            <Image
+                                src={logo}
+                                alt={`Brand ${index+1}`}
+                                className='w-[100px] h-fit'
+                            />
+                        </Card>
+                    ))}
+                </div>
+            </div>
+        </section>
+    </div>
   )
 }
 
--- constant/components/index.ts
+++ constant/components/index.ts
@@ -39,6 +39,24 @@ import team2 from "@/public/assets/images/solutions/outsourcing/recruitment.png"
 import team3 from "@/public/assets/images/solutions/outsourcing/security.png"
 import team4 from "@/public/assets/images/solutions/outsourcing/talent_pool.png"
 
+import albalsamlogo from "@/public/assets/images/brand_partners/Al_balsam_international.png"
+import africell_logo from "@/public/assets/images/brand_partners/africell.png"
+import dk_telecom_logo from "@/public/assets/images/brand_partners/dk_telecom.png"
+import gach_global_logo from "@/public/assets/images/brand_partners/gach_global.png"
+import gamcellogo from "@/public/assets/images/brand_partners/gamcel.png"
+import gamtellogo from "@/public/assets/images/brand_partners/gamtel.png"
+import gralogo from "@/public/assets/images/brand_partners/gra.png"
+import humanappeallogo from "@/public/assets/images/brand_partners/human_appeal_ireland.png"
+import merck_foundationlogo from "@/public/assets/images/brand_partners/merck_foundation.png"
+import metzylogo from "@/public/assets/images/brand_partners/metzy.png"
+import qatar_charitylogo from "@/public/assets/images/brand_partners/qatar_charity.png"
+import qcelllogo from "@/public/assets/images/brand_partners/qcell.png"
+import wamylogo from "@/public/assets/images/brand_partners/wamy.png"
+
+export const brands = [albalsamlogo, africell_logo, dk_telecom_logo, gach_global_logo, gamcellogo, gamtellogo, gralogo, humanappeallogo, merck_foundationlogo,
+    metzylogo, qatar_charitylogo, qcelllogo, wamylogo
+]
+
 
 export const team_members = [team1, team2, team3, team4]
 
