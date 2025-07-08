9cf396e089750828bf25cb0e314d7025ee19bd87
Tulasijoshua <mbawinitulasi12@gmail.com>
2025-07-08 21:43:04 +0000

    feat: change mobile app

--- components/solutions/mobile-applications/mobile-apps/mobile-apps-specification.tsx
+++ components/solutions/mobile-applications/mobile-apps/mobile-apps-specification.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react'
-import mobileAppImg from "@/public/assets/images/digital-solutions/mobileApp_img.png"
+import mobileAppImg from "@/public/assets/images/digital-solutions/mobile_app_img.png"
 import Image from 'next/image'
 import Typography from '@/components/shared/typography'
 import { Button } from '@/components/ui/button';
